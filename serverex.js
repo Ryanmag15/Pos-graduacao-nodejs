49e5f4a3506a45896794b109e3d05871071cc839
Ryan Mathias <ryanmag15@gmail.com>
2023-11-28 20:13:14 -0300

    Alteração da porta

--- serverex.js
+++ serverex.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 const apiV1Router = require('./routes/apiV1Router');
 app.use('/api/v1', apiV1Router);
 
-const apiV2Router = require ('./routes/apiV2Router')
+const apiV2Router = require('./routes/apiV2Router');
 app.use('/api/v2', apiV2Router);
 
 app.use((req, res, next) => {
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
