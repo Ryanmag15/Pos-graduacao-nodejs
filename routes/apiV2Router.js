9779dca59c57aaf373e2fa4b7bf121a84abe4c35
Ryan Mathias <ryanmag15@gmail.com>
2023-11-28 21:44:36 -0300

    Criação de usuários

--- routes/apiV2Router.js
+++ routes/apiV2Router.js
@@ -5,76 +5,77 @@ const knex = require('knex')(knexConfig);
 
 apiV2Router.get('/', (req, res) => {
   res.send(`API V2<br>
-        <a href="/api/v2/produtos">API de Produtos</a>`);
+        <a href="/api/v2/users">API de Users</a>`);
 });
 
-apiV2Router.get('/produtos', async (req, res) => {
+apiV2Router.get('/users', async (req, res) => {
   try {
-    const result = await knex.select('id', 'descricao', 'marca', 'valor').from('produtos');
+    const result = await knex.select('id', 'login', 'senha', 'nome', 'numero').from('users');
     res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({ erro: 'Erro ao recuperar os produtos do banco de dados.' });
+    res.status(500).json({ erro: 'Erro ao recuperar os usuários do banco de dados.' });
   }
 });
 
-apiV2Router.get('/produtos/:id', async (req, res) => {
+apiV2Router.get('/users/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const produto = await knex.select('id', 'descricao', 'marca', 'valor').from('produtos').where({ id: id }).first();
-    if (produto) {
-      res.status(200).json(produto);
+    const user = await knex.select('id', 'login', 'senha', 'nome', 'numero').from('users').where({ id: id }).first();
+    if (user) {
+      res.status(200).json(user);
     } else {
-      res.status(404).json({ erro: 'Produto não encontrado.' });
+      res.status(404).json({ erro: 'Usuário não encontrado.' });
     }
   } catch (error) {
-    res.status(500).json({ erro: 'Erro ao recuperar o produto do banco de dados.' });
+    res.status(500).json({ erro: 'Erro ao recuperar o usuário do banco de dados.' });
   }
 });
 
-apiV2Router.post('/produtos', async (req, res) => {
-  const novoProduto = req.body;
+apiV2Router.post('/users', async (req, res) => {
+  const newUser = req.body;
 
   try {
-    const [id] = await knex('produtos').insert(novoProduto);
-    const produtoInserido = await knex.select('id', 'descricao', 'marca', 'valor').from('produtos').where({ id: id }).first();
-    res.status(201).json({ mensagem: 'Produto adicionado com sucesso.', produto: produtoInserido });
+    const [id] = await knex('users').insert(newUser);
+    const userInserted = await knex.select('id', 'login', 'senha', 'nome', 'numero').from('users').where({ id: id }).first();
+    res.status(201).json({ mensagem: 'Usuário adicionado com sucesso.', user: userInserted });
   } catch (error) {
-    res.status(400).json({ erro: 'Os dados do produto são obrigatórios.' });
+    res.status(400).json({ erro: 'Os dados do usuário são obrigatórios.' });
   }
 });
 
-apiV2Router.put('/produtos/:id', async (req, res) => {
+apiV2Router.put('/users/:id', async (req, res) => {
   const { id } = req.params;
-  const dadosAtualizados = req.body;
+  const updatedData = req.body;
 
   try {
-    const quantidadeAtualizada = await knex('produtos').where({ id: id }).update(dadosAtualizados);
-    if (quantidadeAtualizada > 0) {
-      const produtoAtualizado = await knex.select('id', 'descricao', 'marca', 'valor').from('produtos').where({ id: id }).first();
-      res.status(200).json({ mensagem: 'Produto atualizado com sucesso.', produto: produtoAtualizado });
+    const updatedQuantity = await knex('users').where({ id: id }).update(updatedData);
+    if (updatedQuantity > 0) {
+      const userUpdated = await knex.select('id', 'login', 'senha', 'nome', 'numero').from('users').where({ id: id }).first();
+      res.status(200).json({ mensagem: 'Usuário atualizado com sucesso.', user: userUpdated });
     } else {
-      res.status(404).json({ erro: 'Produto não encontrado.' });
+      res.status(404).json({ erro: 'Usuário não encontrado.' });
     }
   } catch (error) {
-    res.status(500).json({ erro: 'Erro ao atualizar o produto no banco de dados.' });
+    res.status(500).json({ erro: 'Erro ao atualizar o usuário no banco de dados.' });
   }
 });
 
-apiV2Router.delete('/produtos/:id', async (req, res) => {
+apiV2Router.delete('/users/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
-    const produtoRemovido = await knex.select('id', 'descricao', 'marca', 'valor').from('produtos').where({ id: id }).first();
-    const quantidadeRemovida = await knex('produtos').where({ id: id }).del();
-    if (quantidadeRemovida > 0) {
-      res.status(200).json({ mensagem: 'Produto removido com sucesso.', produto: produtoRemovido });
+    const userRemoved = await knex.select('id', 'login', 'senha', 'nome', 'numero').from('users').where({ id: id }).first();
+    const removedQuantity = await knex('users').where({ id: id }).del();
+    if (removedQuantity > 0) {
+      res.status(200).json({ mensagem: 'Usuário removido com sucesso.', user: userRemoved });
     } else {
-      res.status(404).json({ erro: 'Produto não encontrado.' });
+      res.status(404).json({ erro: 'Usuário não encontrado.' });
     }
   } catch (error) {
-    res.status(500).json({ erro: 'Erro ao remover o produto do banco de dados.' });
+    res.status(500).json({ erro: 'Erro ao remover o usuário do banco de dados.' });
   }
 });
+
 
 module.exports = apiV2Router;
 
