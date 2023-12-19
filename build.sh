3d7754ae4253034f2acc041dce6bf2f41e55ca0c
Ryan Mathias <ryanmag15@gmail.com>
2023-12-19 19:58:35 -0300

    cccc

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
-    #!/bin/bash
+#!/bin/bash
 
-    # Navigate to the backend directory
-    cd back
-    npm install
-    npx nodemon serverex.js &
+# Navigate to the backend directory
+cd back
+npm install
+npx nodemon serverex.js &
+# Sleep for a while to ensure backend is up before starting the frontend
+sleep 10
 
-    cd front
-    npm install
-    npm start
+# Navigate to the frontend directory
+cd ../front
+npm install
+npm start
